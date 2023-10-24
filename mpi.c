f18e7f3e3e167ca4f48c2a878c16e418d4d3897f
marcoscoutinhodev@outlook.com <marcoscoutinhodev@outlook.com>
2023-10-23 21:55:21 -0300

    v4

--- mpi.c
+++ mpi.c
@@ -1,58 +1,85 @@
-#include <mpi.h>
+
 #include <stdio.h>
-#include <stdlib.h>
+#include <mpi.h>
 #include <time.h>
 
-#define SIZE 700
+#define SIZE 600
 
-int matrixA[SIZE][SIZE];
-int matrixB[SIZE][SIZE];
-int matrixC[SIZE][SIZE];
+int main(int argc, char **argv) {
+  int matrixA[SIZE][SIZE];
+  int matrixB[SIZE][SIZE];
+  int matrixC[SIZE][SIZE];
+  int localA[SIZE][SIZE];
+  int localC[SIZE][SIZE];
 
-int main(int argc, char *argv[])
-{
-  int i, j, k;
-  int rank, size;
+  // Initialize the MPI environment
+  MPI_Init(NULL, NULL);
 
-  MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  // Get the number of processes
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-  if (rank == 0)
-  {
-    for (i = 0; i < SIZE; i++)
-    {
-      for (j = 0; j < SIZE; j++)
-      {
+  // Get the rank of the process
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+  // Get the name of the processor
+  char processor_name[MPI_MAX_PROCESSOR_NAME];
+  int name_len;
+  MPI_Get_processor_name(processor_name, &name_len);
+
+  //clock_t start = clock();
+
+  if (world_rank == 0) {
+    for (int i = 0; i < SIZE; i++) {
+      for (int j = 0; j < SIZE; j++) {
         matrixA[i][j] = 1;
         matrixB[i][j] = 2;
       }
     }
   }
 
+  // Broadcast matrixB to all processes
+  MPI_Bcast(&matrixB, SIZE*SIZE, MPI_INT, 0, MPI_COMM_WORLD);
+
+  // Scatter rows of matrixA to all processes
+  int sendcount = SIZE*SIZE/world_size;
+  MPI_Scatter(&matrixA, sendcount, MPI_INT, &localA,
+              sendcount, MPI_INT, 0, MPI_COMM_WORLD);
+
   clock_t start = clock();
-  MPI_Bcast(matrixA, SIZE * SIZE, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(matrixB, SIZE * SIZE, MPI_INT, 0, MPI_COMM_WORLD);
-
-
-  for (i = rank; i < SIZE; i += size)
-  {
-    for (j = 0; j < SIZE; j++)
-    {
-      matrixC[i][j] = 0.0;
-      for (k = 0; k < SIZE; k++)
-        matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
+  // Each process performs its computation
+  for (int i = 0; i < SIZE/world_size; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      localC[i][j] = 0;
+      for (int k = 0; k < SIZE; k++)
+        localC[i][j] += localA[i][k] * matrixB[k][j];
     }
   }
 
-  if (rank == 0)
-  {
-    clock_t end = clock();
-    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("Tempo de execução paralelo: %f\n", time_spent);
-  }
+  clock_t end = clock();
+  double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+  printf("%s: %f\n", processor_name, time_spent);
 
+  // Gather all partial results to the final result matrix
+  MPI_Gather(&localC, sendcount, MPI_INT, &matrixC,
+            sendcount, MPI_INT, 0, MPI_COMM_WORLD);
+
+  /*if (world_rank == 0) {
+
+    for (int i = 0; i < SIZE; i++) {
+      for (int j = 0; j < SIZE; j++) {
+        printf("%d ", matrixC[i][j]);
+      }
+
+      printf("\n");
+    }
+
+  }*/
+
+  // Finalize the MPI environment.
   MPI_Finalize();
 
   return 0;
 }
+
--- seq.c
+++ seq.c
@@ -2,20 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define SIZE 700
+#define SIZE 600
 
 int matrixA[SIZE][SIZE];
 int matrixB[SIZE][SIZE];
 int matrixC[SIZE][SIZE];
 
-int main()
-{
+int main() {
   int i, j, k;
 
-  for (i = 0; i < SIZE; i++)
-  {
-    for (j = 0; j < SIZE; j++)
-    {
+  for (i = 0; i < SIZE; i++) {
+    for (j = 0; j < SIZE; j++) {
       matrixA[i][j] = 1;
       matrixB[i][j] = 2;
     }
@@ -23,10 +20,8 @@ int main()
 
   clock_t start = clock();
 
-  for (i = 0; i < SIZE; i++)
-  {
-    for (j = 0; j < SIZE; j++)
-    {
+  for (i = 0; i < SIZE; i++) {
+    for (j = 0; j < SIZE; j++) {
       matrixC[i][j] = 0.0;
       for (k = 0; k < SIZE; k++)
         matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
